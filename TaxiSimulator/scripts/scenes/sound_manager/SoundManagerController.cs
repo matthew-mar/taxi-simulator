fa8538b1884e6b047b73776602d288118315d469
matthew-mar <mmary.prog@gmail.com>
2024-04-12 00:04:21 +0300

    звуки мотора

--- TaxiSimulator/scripts/scenes/sound_manager/SoundManagerController.cs
+++ TaxiSimulator/scripts/scenes/sound_manager/SoundManagerController.cs
@@ -2,7 +2,9 @@ using Godot;
 using TaxiSimulator.Common;
 
 using PauseSignals = TaxiSimulator.Scenes.Pause.Signals;
+using CarSignals = TaxiSimulator.Scenes.CarScene.Signals;
 using MainMenuSignals = TaxiSimulator.Scenes.MainMenu.Signals;
+using InputSignals = TaxiSimulator.Scenes.InputController.Signlas;
 
 namespace TaxiSimulator.Scenes.SoundManager {
 	public partial class SoundManagerController : Node {
@@ -14,6 +16,8 @@ namespace TaxiSimulator.Scenes.SoundManager {
 			Instance ??= this;
 
 			var buttonPlayer = Instance.GetNode<AudioStreamPlayer>("ButtonPlayer");
+			var motorPlayer = Instance.GetNode<AudioStreamPlayer>("MotorPlayer");
+			motorPlayer.Playing = true;
 
 			MainMenuSignals.SignalsProvider.PlayButtonPressedSignal.PlayButtonPressed +=
 				(EventSignalArgs args) => {
@@ -33,6 +37,14 @@ namespace TaxiSimulator.Scenes.SoundManager {
 			PauseSignals.SignalsProvider.MainMenuButtonPressed.MainMenuButtonPressed +=
 				(EventSignalArgs args) => {
 					buttonPlayer.Play();
+				};
+
+			CarSignals.SignalsProvider.SpeedChangedSignal.SpeedChanged +=
+				(CarSignals.SpeedSignalArgs args) => {
+					var speedKm = args.CurrentSpeed.Length();
+					if (speedKm > 0) {
+						motorPlayer.PitchScale = 1f + speedKm / 50f;
+					}
 				};
 		}
 	}
