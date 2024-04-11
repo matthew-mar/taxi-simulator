127ddb2d4ec91337b2f01847853ffd2c8e260808
matthew-mar <mmary.prog@gmail.com>
2024-04-11 15:09:55 +0300

    модель реальной машины

--- TaxiSimulator/scripts/scenes/car_scene/view/Car.cs
+++ TaxiSimulator/scripts/scenes/car_scene/view/Car.cs
@@ -4,7 +4,7 @@ using TaxiSimulator.Scenes.CarScene.Signals;
 namespace TaxiSimulator.Scenes.CarScene.View {
 	
 	public partial class Car : VehicleBody3D {
-		public const string NodePath = "car";
+		public const string NodePath = "car_copy_1";
 
 		private Camera3D _backCamera;
 
